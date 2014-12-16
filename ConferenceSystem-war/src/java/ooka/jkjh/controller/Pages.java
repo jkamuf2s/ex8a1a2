571f6796fdcf9ff3ae0b1d879de6951d6dc10efa
jkamuf2s <jochen.kamuf@smail.inf.h-brs.de>
2014-12-16 12:57:32 +0100

    added get redirect

--- ConferenceSystem-war/src/java/ooka/jkjh/controller/Pages.java
+++ ConferenceSystem-war/src/java/ooka/jkjh/controller/Pages.java
@@ -6,11 +6,11 @@ package ooka.jkjh.controller;
  */
 public class Pages {
 
-    public static final String INDEX_PAGE = "index";
-    public static final String SIGN_UP_PAGE = "signup";
-    public static final String USER_ACTION_OVERVIEW = "overview";
-    public static final String CREATE_CONFERENCE = "createconference";
-    public static final String REGISTER_AT_CONFERENCE = "registeratconference";
-    public static final String RATE_A_CONFERENCE = "rateconference";
+    public static final String INDEX_PAGE = "index" + "?faces-redirect=true";
+    public static final String SIGN_UP_PAGE = "signup" + "?faces-redirect=true";
+    public static final String USER_ACTION_OVERVIEW = "overview" + "?faces-redirect=true";
+    public static final String CREATE_CONFERENCE = "createconference" + "?faces-redirect=true";
+    public static final String REGISTER_AT_CONFERENCE = "registeratconference" + "?faces-redirect=true";
+    public static final String RATE_A_CONFERENCE = "rateconference" + "?faces-redirect=true"; 
 
 }
